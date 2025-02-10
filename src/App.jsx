f5b564e23ccd9322711950b26345f44b05a754c4
Vinay-githubb <vinay.ky.07@gmail.com>
2025-02-09 23:49:16 -0500

    third problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import React01 from './One'
-import React2 from './React2'
+// import React2 from './React2'
+import Three from './Three'
 export default function App() {
   return (
     <div>
       {/* <One/> */}
-      <React2/>
+      {/* <React2/> */}
+      <Three/>
   
     </div>
   )
---
+++ src/Three.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+export default function Three() {
+
+const[input, setInput]=useState(" ");
+
+const userInput=(event)=>{
+  setInput(event.target.value);
+}
+
+
+
+  return (
+    <div>
+     <input type="text" onChange={userInput}/>
+     <p>User input: {input}</p>
+    </div>
+  )
+}
